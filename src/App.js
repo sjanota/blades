3da15e65a2b6017cda801708fc8b1560185a05eb
Szymon Janota <szymon.janota@sap.com>
2019-07-28 19:54:09 +0200

    Show modal

--- src/App.js
+++ src/App.js
@@ -3,57 +3,57 @@ import './App.css';
 import ImageMapper from 'react-image-mapper';
 import doskvolMap from './doskvol-map.jpg';
 import map from './const/map';
+import ReactModal from 'react-modal';
 
 const width = 1000;
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            selectedArea: null
+        }
     }
 
     render() {
+        const {selectedArea} = this.state;
+        console.log("Selected area", this.state.selectedArea);
         return (
             <div className="App">
-                    <ImageMapper src={doskvolMap} map={map}
-                                 imgWidth={2192} imgHeight={1648}
-                                 width={width} height={1648*width/2192}
-                                 onLoad={this.load}
-                                 onClick={this.clicked}
-                                 onMouseEnter={this.enterArea}
-                                 onMouseLeave={this.leaveArea}
-                                 onMouseMove={this.moveOnArea}
-                                 onImageClick={this.clickedOutside}
-                                 onImageMouseMove={this.moveOnImage}
-                    />
+                <ReactModal
+                    isOpen={selectedArea != null}
+                    onRequestClose={this.deselectArea.bind(this)}
+                    style={{
+                        content: {
+                            top: '5%',
+                            left: '20%',
+                            right: '20%',
+                            bottom: 'auto',
+                        }
+                    }}
+                >
+                    <h3>{selectedArea}
+                    </h3>
+                </ReactModal>
+                <ImageMapper
+                    src={doskvolMap} map={map}
+                    imgWidth={2192} imgHeight={1648}
+                    width={width} height={1648 * width / 2192}
+                    onLoad={this.load}
+                    onClick={this.selectArea.bind(this)}
+                />
             </div>
         );
     }
 
-    clicked(area, _, event) {
+    selectArea(area, _, event) {
+        this.setState({selectedArea: area.name});
         console.log("clicked", area)
     }
 
-    load() {
-        console.log("load")
+    deselectArea() {
+        this.setState({selectedArea: null})
     }
-
-    enterArea(area) {
-        console.log("enterArea", area)
-    }
-
-    leaveArea(area) {
-        console.log("leaveArea", area)
-    }
-
-    moveOnArea(area) {
-        console.log("moveOnArea", area)
-    }
-
-    clickedOutside(area) {
-        console.log("clickedOutside", area)
-    }
-
 }
 
 export default App;
