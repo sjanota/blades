4e4d69dd9517f7fff2c97abc2ac4f45663919fb3
Szymon Janota <szymon.janota@sap.com>
2019-07-28 20:18:37 +0200

    Updates

---
+++ precache-manifest.061db18c0cf665775218d50c7d6e8255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b63a1b35ddd9b0cff3ed17b1ee93eed",
+    "url": "/blades/index.html"
+  },
+  {
+    "revision": "4c5b223f4c8a268af572",
+    "url": "/blades/static/css/main.ebecba1b.chunk.css"
+  },
+  {
+    "revision": "2f2a0347ffc8d340e349",
+    "url": "/blades/static/js/2.7acbe17c.chunk.js"
+  },
+  {
+    "revision": "4c5b223f4c8a268af572",
+    "url": "/blades/static/js/main.5f7bf5f1.chunk.js"
+  },
+  {
+    "revision": "41677052de2f24c6c642",
+    "url": "/blades/static/js/runtime~main.69a95b48.js"
+  },
+  {
+    "revision": "f5c67ae59a0abbd6bf47e6cd5d9da51b",
+    "url": "/blades/static/media/doskvol-map.f5c67ae5.jpg"
+  }
+]);
