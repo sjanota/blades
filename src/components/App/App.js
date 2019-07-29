6226f1134fa5931d99a3bfe18e060cb5fb7c0675
Szymon Janota <szymon.janota@sap.com>
2019-07-29 19:43:36 +0200

    Move everything to components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import CityMap from "./CityMap/CityMap";
-import DistrictDetailsModal from "./DistrictDetailsModal/DistrictDetailsModal";
+import CityMap from "../CityMap/CityMap";
+import DistrictDetailsModal from "../DistrictDetailsModal/DistrictDetailsModal";
 
 class App extends React.Component {
     constructor(props) {
