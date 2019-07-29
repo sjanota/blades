81a4756b8965159a945b3bd6b4cda5ca29216644
Szymon Janota <szymon.janota@sap.com>
2019-07-29 19:01:03 +0200

    Responsive map

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import doskvolMap from './doskvol-map.jpg';
 import map from './const/map';
 import ReactModal from 'react-modal';
 
-const width = 1000;
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedArea: null
+            selectedArea: null,
+            width: null
         }
     }
 
+    componentDidMount() {
+
+    }
+
+
     render() {
-        const {selectedArea} = this.state;
+        const {selectedArea, width} = this.state;
         console.log("Selected area", this.state.selectedArea);
         return (
             <div className="App">
@@ -35,12 +39,10 @@ class App extends React.Component {
                     <h3>{selectedArea}
                     </h3>
                 </ReactModal>
-                <ImageMapper
-                    src={doskvolMap} map={map}
-                    imgWidth={2192} imgHeight={1648}
-                    width={width} height={1648 * width / 2192}
-                    onLoad={this.load}
-                    onClick={this.selectArea.bind(this)}
+                <Wrapper
+                    selectArea={this.selectArea.bind(this)}
+                    setSize={this.setSize.bind(this)}
+                    width={width}
                 />
             </div>
         );
@@ -54,6 +56,59 @@ class App extends React.Component {
     deselectArea() {
         this.setState({selectedArea: null})
     }
+
+    setSize(size) {
+        if (size.width !== this.state.width) {
+            console.log("set width", size.width);
+            this.setState({width: size.width})
+        }
+    }
 }
 
 export default App;
+
+class Wrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: null,
+            ref: React.createRef()
+        };
+
+        this.updateWidth = this.updateWidth.bind(this);
+    }
+
+    updateWidth() {
+        const width = this.state.ref.current.clientWidth;
+        if (width !== this.state.width) {
+            this.setState({width});
+        }
+    }
+
+    componentDidMount() {
+        this.updateWidth();
+
+        window.addEventListener('resize', this.updateWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWidth);
+    }
+
+    render() {
+        const {selectArea} = this.props;
+        const {width, ref} = this.state;
+
+        return (
+            <div className={"App-wrapper"} ref={ref}>
+                <ImageMapper
+                    src={doskvolMap} map={map}
+                    imgWidth={2192} imgHeight={1648}
+                    width={width} height={1648 * width / 2192}
+                    onClick={selectArea}
+                />
+            </div>
+        )
+    }
+}
